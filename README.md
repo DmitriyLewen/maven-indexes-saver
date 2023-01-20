75e13655fcf288f729a1f5c8fffbdaf7812e521f
DmitriyLewen <dmitriy.lewen@smartforce.io>
2023-01-20 13:12:03 +0600

    feat: add logger

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This repository parses all `nexus-maven-repository-index.XXX.gz` archives from [maven/.index](https://repo.maven.apache.org/maven2/.index/) and saves them as json files.
 
-You can see `Index` structure for json [here]().
+You can see `Index` structure for json [here](https://github.com/DmitriyLewen/maven-indexes-saver/blob/main/src/main/java/dmitriylewen/maven/indexex/saver/Index.java#L3-L26).
--- src/main/java/dmitriylewen/maven/indexex/saver/IndexesSaver.java
+++ src/main/java/dmitriylewen/maven/indexex/saver/IndexesSaver.java
@@ -1,6 +1,8 @@
 package dmitriylewen.maven.indexex.saver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.maven.index.reader.*;
 import org.apache.maven.index.reader.Record;
 import org.apache.maven.index.reader.resource.UrlResource;
@@ -40,12 +42,18 @@ public class IndexesSaver {
     }
 
     public static void main(String[] args) throws IOException {
+        final Logger logger = Logger.getLogger(IndexesSaver.class);
+        if (args.length != 1 || !Objects.equals(args[0], "DEBUG")) {
+            logger.setLevel(Level.INFO);
+        }
+
         int lastIndex;
         // get the latest index to start parsing from this archive
         try (UrlResource urlResource = new UrlResource(new URL(MAVEN_REPOSITORY_BASE_URL + PROPERTIES_FILE_NAME));
              ResourceHandler remote = new SaverResourceHandler(urlResource)) {
             Properties props = loadProperties(remote.locate(""));
             lastIndex = Integer.parseInt(props.getProperty(LAST_INCREMENTAL_INDEX));
+            logger.info("last index has been found: " + lastIndex);
         }
 
         int numberOfIndexes = 0; // number of indexes found from all archives
@@ -73,20 +81,21 @@ public class IndexesSaver {
                 try {
                     ObjectMapper mapper = new ObjectMapper();
                     mapper.writeValue(new File(path), indexes);
+                    logger.debug("indexes from " + String.format(ARCHIVE_NAME, i) + " has been saved to json file " + path);
                 } catch (IOException e) {
-                    System.out.printf("error: can't save indexes to json: %s\n", e);
+                    logger.error("can't save indexes to json: " + e);
                 }
 
             } catch (FileNotFoundException e) {
                 // maven repository doesn't have some first archives
                 // e.g. from 787 to 161
                 // after got exception - stop parse
-                System.out.println("Parse of archives finished.");
-                System.out.printf("last archive: " + ARCHIVE_NAME + "\n", i - 1);
-                System.out.printf("Saved %d indexes\n", numberOfIndexes);
+                logger.info("Parse of archives finished.");
+                logger.info("Last archive: " + String.format(ARCHIVE_NAME, i - 1));
+                logger.info(String.format("Saved %d indexes", numberOfIndexes));
                 break;
             } catch (IOException e) {
-                System.out.printf("error: %s\n", e);
+                logger.error("unexpected error: " + e);
             }
 
         }
